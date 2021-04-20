0c14706ee21f6a09cd787b6a4c098c343e612e08
Donte <andrewdante2@gmail.com>
2021-04-20 10:46:44 +0800

    Added BoundingBox

--- BoundingSphere.c
+++ BoundingSphere.c
@@ -1,6 +1,6 @@
 #include "BoundingSphere.h"
 
-bool isCollide(BoundingSphere sphere1, BoundingSphere sphere2)
+bool isSphereCollide(BoundingSphere sphere1, BoundingSphere sphere2)
 {
     float radiusDistance = sphere1.radius + sphere2.radius;
     float centerDistance = Magnitude(Subtract(sphere2.center, sphere1.center));
